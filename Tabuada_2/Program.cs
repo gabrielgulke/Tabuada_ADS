6d79b05878fca6be1e2457b19243c57b61699f54
Gabriel Gulke <ggulke070@gmail.com>
2022-06-19 14:09:27 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número da tabuada: ");
+            int ntab = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual número você deseja se se inicie a tabuada?: ");
+            int ni = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual número você quer que termine a tabuada?: ");
+            int nf = Convert.ToInt32(Console.ReadLine());
+            for (int n = ni; n <= nf; n++)
+                 
+                        
+            Console.WriteLine(ntab + "x" + n + "=" + n * ntab);
+            
+         
+            
+            Console.ReadKey();
+        }
+    }
+}
